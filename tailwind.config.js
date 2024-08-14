8563a10f664a02134e492e2b99e5ab68c3c93ba2
April Smith <april.smith@ably.com>
2024-08-14 12:20:38 +0100

    Fix: Indifinte lopp with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ const safelistGreedy = [/^docs-.*/, /^col-span-.*/, /^rotate-/];
 
 module.exports = extendConfig((ablyUIConfig) => ({
   ...ablyUIConfig,
-  content: ['./src/**/*.{js,jsx,ts,tsx}', ...ablyUIConfig.content],
+  content: ['./src/pages/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}', ...ablyUIConfig.content],
   safelist: {
     ...ablyUIConfig.safelist,
     ...Object.keys(apiReferenceSpecificColors).map((c) => `bg-${c}`),
