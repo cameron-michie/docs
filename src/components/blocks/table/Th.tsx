87abeb64402888e8fb22d541098ec752ca07bec6
April Smith <april.smith@ably.com>
2024-08-16 17:22:15 +0100

    WIP: font weight

--- src/components/blocks/table/Th.tsx
+++ src/components/blocks/table/Th.tsx
@@ -8,7 +8,7 @@ import { theader } from './Th.module.css';
 const Th = (props: HtmlComponentProps<'th'>) => {
   return (
     <th
-      className={cn('px-16 uppercase font-normal text-menu1 pt-40 pb-20 sticky top-0 bg-white', theader)}
+      className={cn('px-16 uppercase text-menu1 pt-40 pb-20 sticky top-0 bg-white', theader)}
       {...props}
     />
   );
